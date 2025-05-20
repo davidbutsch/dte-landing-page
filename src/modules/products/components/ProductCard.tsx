f027f1f26da5657b1376688661d4185adb8a2106
David <102764748+davidbutsch@users.noreply.github.com>
2025-05-20 18:47:28 -0500

    add dynamic product card feature chips

--- src/modules/products/components/ProductCard.tsx
+++ src/modules/products/components/ProductCard.tsx
@@ -1,10 +1,10 @@
+import { Product } from "@/modules/products";
 import {
   ProductCardCTA,
   ProductCardFeatures,
 } from "@/modules/products/components";
-import { Product } from "@/modules/stripe";
 import { theme } from "@/theme";
-import { Grid2, Paper, Stack, Typography } from "@mui/material";
+import { Chip, Grid2, Paper, Stack, Typography } from "@mui/material";
 
 export type ProductCardOptions = {
   product: Product;
@@ -14,16 +14,23 @@ export const ProductCard = ({ product }: ProductCardOptions) => {
   return (
     <Grid2 size={{ xs: 12, sm: 6, md: 4 }} key={product.id}>
       <Paper variant="outlined" sx={{ padding: theme.spacing(2) }}>
-        <Stack mb={2}>
+        <Stack mb={2} gap={2}>
+          {product.metadata?.mostPopular == "yes" && (
+            <Chip
+              label="Most Popular"
+              color="success"
+              variant="filled"
+              size="small"
+              sx={{
+                width: "fit-content",
+              }}
+            />
+          )}
           <Typography variant="h5" fontWeight={600}>
             {product.name}
           </Typography>
-          <Typography mb={2} color="textSecondary">
-            {product.description}
-          </Typography>
-          <Typography variant="h4" fontWeight={600}>
-            {product.defaultPrice?.displayText}
-          </Typography>
+          {/* TODO */}
+          <Typography>{product.defaultPriceId}</Typography>
         </Stack>
 
         <ProductCardCTA productId={product.id} />
