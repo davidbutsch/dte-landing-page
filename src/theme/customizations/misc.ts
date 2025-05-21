683d360adc1dcc0129e374f498545d3335115de0
David <102764748+davidbutsch@users.noreply.github.com>
2025-05-21 14:16:31 -0500

    remove box shadow from chip component

--- src/theme/customizations/misc.ts
+++ src/theme/customizations/misc.ts
@@ -6,7 +6,6 @@ export const miscCustomizations: Components<Theme> = {
     styleOverrides: {
       root: {
         borderRadius: 0,
-        boxShadow: "2px 2px 0px 1px #000",
         outline: "1px solid #000",
       },
     },
