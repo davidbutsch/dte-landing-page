2fb321866afeb161e198fa156bc06e84d31456de
David <102764748+davidbutsch@users.noreply.github.com>
2025-05-21 22:54:32 -0500

    automatically augment pallete colors

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -1,24 +1,22 @@
-import { createTheme, PaletteOptions } from "@mui/material";
+import { createTheme } from "@mui/material";
 
 const { augmentColor } = createTheme().palette;
 
 const createColor = (mainColor: string) =>
   augmentColor({ color: { main: mainColor } });
 
-export const palette: PaletteOptions = {
-  primary: { main: "#F54952" },
-  secondary: { main: "#422680" },
+export const palette = {
+  primary: createColor("#F54952"),
+  secondary: createColor("#422680"),
   cream: createColor("#FAF5EA"),
-  success: {
-    main: "#50C878",
-  },
+  success: createColor("#50C878"),
   text: {
     primary: "#000",
-    secondary: "#1a1a1a",
+    secondary: "#1A1A1A",
   },
   background: {
     default: "#FAF5EA",
-    paper: "#f7efde",
+    paper: "#F7EFDE",
   },
   divider: "#000",
 };
