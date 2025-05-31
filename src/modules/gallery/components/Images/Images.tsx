e440a589889db6f201ed7210a5645f016d48206c
David <102764748+davidbutsch@users.noreply.github.com>
2025-05-31 01:02:42 -0500

    add border box to gallery images

--- src/modules/gallery/components/Images/Images.tsx
+++ src/modules/gallery/components/Images/Images.tsx
@@ -37,14 +37,23 @@ export const Images = () => {
   return (
     <Container>
       <ImageList
-        sx={{ width: "100%", height: "100%", overflow: "hidden", zIndex: -1 }}
+        sx={{ width: "100%", height: "100%", overflow: "visible", zIndex: -1 }}
+        gap={30}
         variant="masonry"
         dir="row"
         cols={3}
       >
         {paths?.map((path, i) => (
           <ImageListItem key={`${path}-${i}`}>
-            <img src={path} alt={`image-${i}`} loading="lazy" />
+            <img
+              src={path}
+              alt={`image-${i}`}
+              loading="lazy"
+              style={{
+                boxShadow: "4px 4px 0px 1px #000",
+                outline: "1px solid #000",
+              }}
+            />
           </ImageListItem>
         ))}
       </ImageList>
