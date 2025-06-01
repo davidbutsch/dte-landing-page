86875d5395a5f32cb34bc16dd279b6aad55bb468
David <102764748+davidbutsch@users.noreply.github.com>
2025-05-31 20:02:27 -0500

    add checkout redirect page

--- src/modules/checkout/routes/RedirectPage.tsx
+++ src/modules/checkout/routes/RedirectPage.tsx
@@ -1,3 +1,34 @@
+import { Box, Icon, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
 export const RedirectPage = () => {
-  return <div>Success? Maybe?</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 2,
+
+        height: "50vh",
+      }}
+    >
+      <Icon
+        className="material-symbols-outlined"
+        sx={{ fontSize: 72 }}
+        color="success"
+      >
+        check_circle
+      </Icon>
+      <span>
+        <Typography variant="h5" fontWeight="bold">
+          Payment Successful
+        </Typography>
+        <Typography variant="subtitle1">
+          Navigate to the <Link to="/calendar">Calendar</Link> to view program
+          schedule.
+        </Typography>
+      </span>
+    </Box>
+  );
 };
