23a1594174e759cba41d278c5b9e6c5654fa39e7
David <102764748+davidbutsch@users.noreply.github.com>
2025-06-12 23:29:44 -0500

    fix copyright date on footer

--- src/modules/layout/components/Footer/CopyrightTypography.tsx
+++ src/modules/layout/components/Footer/CopyrightTypography.tsx
@@ -2,6 +2,6 @@ import { Typography } from "@mui/material";
 
 export const CopyrightTypography = () => {
   return (
-    <Typography variant="caption">2024 © Dream Team Sports Group</Typography>
+    <Typography variant="caption">2025 © Dream Team Sports Group</Typography>
   );
 };
